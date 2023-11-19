bfe1be9bea429e4fa2246b41a3b88e4a323d3ffd
cjheyde <carlaheyde@gmail.com>
2023-11-19 04:52:30 +0100

    feat: :construction: add Header, Register and AuxButton

---
+++ src/Register.tsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function Register() {
+  return (
+    <section className="simpleText">
+      <a href="register.html">Register</a>
+    </section>
+  );
+}
+
+export default Register;
